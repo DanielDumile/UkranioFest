85790c0cd2fffb7b733a0af14f09267077d7bd6a
DanielDumile <daniel_gn98@hotmail.com>
2019-11-29 15:48:23 -0600

    Update mainServer.cpp

--- Proyecto/mainServer.cpp
+++ Proyecto/mainServer.cpp
@@ -50,7 +50,7 @@ int main(int argc, char* argv[]){
         memset(datos, 0, TAM_DATOS);
         memcpy(datos, m.arguments, sizeof(char) * TAM_DATOS);
 
-        printf("datos: %s\n", datos);
+        //printf("datos: %s\n", datos);
 
         bool found = std::binary_search(vec.begin(), vec.end(), datos);
 
@@ -60,103 +60,3 @@ int main(int argc, char* argv[]){
     
     return 0;
 }
-
-// int main(){
-//     struct timeval stamp;
-//     //Se inicia el servidor
-//     Respuesta respuesta(7200);
-//     long int segundos;
-//     long int usegundos;
-//     std::cout << "Servidor Iniciado...\n" << std::endl;
-//     int num[2], aux;
-//     int* opOperation;
-//     long int *op;
-//     mensaje resp;
-//     int nbd = 0, aux2, id = 0, idAux;   
-//     char cTimeStamp[11];
-//     int archivo;
-//     struct registro reg1, reg2;
-//     long t, t2;
-//     string Snumeros;
-//     int flag = 0;
-
-//     vector<string> numeros;
-//     struct TrieNode *root = getNode(); 
-//     char temp[11];
-
-
-//     cout << "Esperando...\n" << std::endl;
-
-//     archivo = open("registros2.txt", O_WRONLY | O_TRUNC | O_CREAT, 0666);
-
-//     if( archivo == -1 ) {
-//         printf("Error no se puede abrir el archivo\n");
-//     }
-
-
-//     while(1){
-
-//         //Recibe la respuesta del cliente
-//         memcpy(&resp,(respuesta.getRequest()),sizeof(mensaje));
-//         gettimeofday(&stamp, NULL);
-//         segundos = static_cast<long int> (stamp.tv_sec);
-//         usegundos = static_cast<long int>  (stamp.tv_usec);
-//         segundos += usegundos;        
-//         //std::cout << "Cliente: \n" <<  "IP:" << inet_ntoa(respuesta.getSocket()->getDirForanea().sin_addr);
-//         //std::cout << ", Puerto: " << ntohs(respuesta.getSocket()->getDirForanea().sin_port) << "\n" << std::endl;
-
-//         memcpy(&idAux,&resp.requestId,sizeof(unsigned int));
-//         memcpy(&opOperation,&resp.operationId,sizeof(int));
-//         memcpy(&reg1,&resp.arguments,sizeof(reg1));
-//         //itoa(segundos, cTimeStamp, 10);
-//         sprintf(cTimeStamp, "%ld", segundos);
-//         //printf("%s\n", cTimeStamp);
-//         memcpy(&reg1.timestamp, &cTimeStamp, sizeof(cTimeStamp));
-//         //memcpy(&aux2,&resp.arguments,sizeof(reg1));
-//         // memcpy(&reg1,paqueteRecepcion.obtieneDatos(),sizeof(reg1));
-//         //printf("Numero registro: %s \t numero atoi: %d\n", reg1.celular, (int) reg1.celular);  
-
-//         // if(id == 0){
-//         //     memcpy(temp,&reg1.celular, sizeof(reg1.celular));
-                       
-//         // }             
-
-//         for (int i = 0; i < numeros.size(); i++){
-//             insert(root, numeros[i]); 
-//         }
-
-//         if(search(root, reg1.celular)){            
-//             flag = 1;
-//             segundos = 0;
-//             printf("Número repetido...\n");
-//         }else{               
-//             numeros.push_back(reg1.celular);        
-//         }
-//         // if(id == 999){ //Indicar el id 
-//         //     if(search(root, temp)){            
-//         //         flag = 1;
-//         //         segundos = 0;
-//         //         printf("Número repetido...\n");
-//         //     }
-//         // }
-//         printf("Paquete: %d\n", id);
-
-//         if (idAux == id && flag == 0) {            
-//             write(archivo, &reg1, sizeof(reg1));
-//             //fsync(archivo);
-//             printf("Se recibio y se escribio en la BD: \n");
-//             write(1, &reg1, sizeof(reg1));        
-//             printf("\n");
-//             id++;
-//         }
-//         //nbd = 1;
-//         flag = 0;
-//         op = (&segundos);
-//         respuesta.sendReply((char*)op);
-//     }
-
-//     close(archivo);
-    
-//     return 0;
-// }
-
